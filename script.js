f277d6909770041c3ec1756a8b3c9cef6d7c402b
Vinith Krishnan <vinithkdckap@gmail.com>
2023-12-02 16:09:03 +0530

    add filter and icon changed in js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let filterCategory = document.querySelectorAll(".filter-category");
+
 let openType = document.querySelector(".type");
 
 for (let i = 0; i < filterCategory.length; i++) {
-    
-    filterCategory[i].addEventListener("click",()=>{ 
-        openType.classList.toggle("open"); 
+    filterCategory[i].addEventListener("click", () => {
+        openType.classList.toggle("open");
     })
-    
+
 }
 
 
@@ -17,31 +17,44 @@ let list = document.querySelector(".product-listing");
 let gridIcon = document.querySelector(".grid");
 let grid = document.querySelector(".grid-products");
 
-listIcon.addEventListener("click",()=>{
-    list.style.display ="block";
-    grid.style.display="none";
-    gridIcon.style.color="black";
-    listIcon.style.color="#740000";
-    
+listIcon.addEventListener("click", () => {
+    list.style.display = "block";
+    grid.style.display = "none";
+    gridIcon.style.color = "#BABABA";
+    listIcon.style.color = "#740000";
+
 })
 
-gridIcon.addEventListener("click",()=>{
-    grid.style.display ="grid";
-    list.style.display="none";
-    gridIcon.style.color="#740000";
-    listIcon.style.color="black";
+gridIcon.addEventListener("click", () => {
+    grid.style.display = "grid";
+    list.style.display = "none";
+    gridIcon.style.color = "#740000";
+    listIcon.style.color = "#BABABA";
 
 
 })
 
 
 // filter btn opne
-
 let filterBtn = document.querySelector(".filterBtn-section");
 let filterContents = document.querySelector(".filter-section");
 
 
-
-filterBtn.addEventListener("click",()=>{
+filterBtn.addEventListener("click", () => {
     filterContents.classList.toggle("filterOpen")
+
+
+
+    // add filter name and icon changing
+    let textContent = document.querySelector('.show-filter');
+    let addIcon = document.querySelector('.material-symbols-outlined');
+
+    if (textContent.innerText == "SHOW FILTERS") {
+        textContent.innerText = "HIDE FILTER";
+        addIcon.innerText = "remove";
+    }
+    else {
+        textContent.innerText = "SHOW FILTERS";
+        addIcon.innerText = "add";
+    }
 })
