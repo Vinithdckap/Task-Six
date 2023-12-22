feb57b5b83ea41d71a6227ede1fac3321daf7b97
Vinith Krishnan <vinithkdckap@gmail.com>
2023-12-22 12:12:48 +0530

    completed plp jquery functionalities tab,accordion

--- script.js
+++ script.js
@@ -55,10 +55,19 @@ $(document).ready(function () {
 })
 
 
+// like btn fill
+
+$('.heart').each(function(){
+    $(this).click(function () {
+        $(this).toggleClass('filled')
+    })
+})
+
+
 
 // read more less functions
 $('.readMore').click(function () {
-    $('.moreText').toggle(1000);
+    $('.moreText').toggle(800);
     if ($('.readMore').text() == "Read More") {
         $(this).text("Read less");
     } else {
@@ -69,17 +78,35 @@ $('.readMore').click(function () {
 
 // learnmore functions
 
-$('.learnMore').click(function () {
-    $('.learnText').toggle(1000);
-    if ($('.learnMore').text() == "Learn More") {
+$(document).ready(function() {
+    $('.learnMore').click(function(e) {
+      e.stopPropagation(); // Stop the event from propagating further
+  
+      let $prevElement = $(this).prev('.learnText');
+  
+      if ($(this).text() === "Learn More") {
+        $('.learnText').not($prevElement).slideUp(); // Close other elements
+        $prevElement.slideToggle(1000); // Toggle the visibility
         $(this).text("Learn Less");
-    }
-    else {
+      } else {
+        $prevElement.slideUp(); // Hide the previous element
         $(this).text("Learn More");
-    }
+      }
+    });
+  
+    // // Clicking anywhere on the document will close all elements
+    $(document).click(function() {
+      $('.learnText').slideUp();
+      $('.learnMore').text("Learn More");
+    });
+  
+    // // Prevent clicking on .learnText from triggering the document click handler
+    $('.learnText').click(function(e) {
+      e.stopPropagation();
+    });
+  });
 
-})
-
+  
 
 
 
